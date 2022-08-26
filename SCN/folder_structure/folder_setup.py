486e55a0a6b1f1b37d9372995582294e3016452e
wmda <p3dh0001@gmail.com>
2022-08-26 10:50:40 +0100

    script works to set up file directory

--- SCN/folder_structure/folder_setup.py
+++ SCN/folder_structure/folder_setup.py
@@ -5,18 +5,24 @@ import argparse
 
 def set_arguments() -> dict:
     option = argparse.ArgumentParser()
-    option.add_argument("-p", "--path", dest='path', help="filepath to set ")
-    
+    option.add_argument("-p", "--path", dest='path', help="filepath to set up directories in")
+    option.add_argument('-n', '--name', dest='name', help='Name of directory. Default is SCN')
     args = vars(option.parse_args())
     return args
 
 
-def folder_creation(file_path : str) -> None:
+def folder_creation(file_path : str, name:str = 'SCN') -> None:
         
         try:
-            os.mkdir(file_path)
+            os.mkdir(f'{file_path}/{name}')
+            os.mkdir(f'{file_path}/{name}/logs')
+            os.mkdir(f'{file_path}/{name}/results')
+            os.mkdir(f'{file_path}/{name}/work')
+            os.mkdir(f'{file_path}/{name}/work/pickle')
+            os.mkdir(f'{file_path}/{name}/work/data')
+
         except PermissionError:
-            print('Do not have the write permissions to create a folder in this directory. Either change file path or the directories permssions')
+            print("Do not have the write permissions to create a folder in this directory. Either change file path or the directory's permssions")
             sys.exit(1)
 
 
@@ -26,9 +32,18 @@ if __name__ == '__main__':
     args = set_arguments()
 
     try:
-        assert os.path.exists(args['path'])
+        if args['name'] == None:
+            assert os.path.exists(args['path']+ '/SCN')
+        if args['name'] != None:
+            assert os.path.exists(args['path']+ '/' + args['name'])
+        
         print('Directory exists, skipping...')
     
     except AssertionError:
-        print(args['path'], ' does not exist. Creating folder now')
-        folder_creation(args['path'])
+        print(args['path'], ' does not exist. Setting up directories now')
+        
+        if args['name'] != None:
+            folder_creation(args['path'], args['name'])
+
+        if args['name'] == None:
+            folder_creation(args['path'])
