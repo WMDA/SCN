b2e9cd533ff532a8a837a0073846708e5e06033c
wmda <p3dh0001@gmail.com>
2022-09-01 12:40:04 +0100

    added exception handling into load pickle object

--- SCN/graphs/graph_utlis.py
+++ SCN/graphs/graph_utlis.py
@@ -5,15 +5,19 @@ from decouple import config
 
 def save_pickle(name: str, object_to_pickle) -> None:
 
-    pickel_path = os.path.join(config('root'), 'work/pickle')
+    pickle_path = os.path.join(config('root'), 'work/pickle')
 
-    with open(f'{pickel_path}/{name}.pickle', 'wb') as handle:
+    with open(f'{pickle_path}/{name}.pickle', 'wb') as handle:
         pickle.dump(object_to_pickle, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 def load_pickle(name_of_pickle_object: str) -> object:
 
-    pickel_path = os.path.join(config('root'), 'work/pickle')
+    pickle_path = os.path.join(config('root'), 'work/pickle')
 
-    with open(f'{pickel_path}/{name_of_pickle_object}.pickle', 'rb') as handle:
-        return pickle.load(handle)
+    try: 
+        with open(f'{pickle_path}/{name_of_pickle_object}.pickle', 'rb') as handle:
+            return pickle.load(handle)
+    
+    except FileNotFoundError:
+        print(f'Unable to load pickle file {pickle_path}/{name_of_pickle_object} Please check the pickle object exists')
