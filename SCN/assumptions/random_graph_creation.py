61d192c8455c7a574878d439df44ca56948b627a
wmda <p3dh0001@gmail.com>
2022-09-01 10:56:13 +0100

    added function documentation

--- SCN/assumptions/random_graph_creation.py
+++ SCN/assumptions/random_graph_creation.py
@@ -5,9 +5,9 @@ class Random_Graph_Creator:
     def __init__(self) -> None:
         pass
 
-    def data(self, *kargs) -> dict:
+    def data(self, *data) -> dict:
 
-        return dict(zip([f'group_{key}'for key in range(len(kargs))], [group for group in kargs]))
+        return dict(zip([f'group_{data}'for key in range(len(data))], [group for group in data]))
 
     #def permutations(self):
 
