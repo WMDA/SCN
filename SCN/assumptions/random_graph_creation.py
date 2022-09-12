1df334497ab8b33fe3f7916b8beec9c7c2d33977
wmda <p3dh0001@gmail.com>
2022-09-12 13:44:08 +0100

    removed uneccesary function argument name

--- SCN/assumptions/random_graph_creation.py
+++ SCN/assumptions/random_graph_creation.py
@@ -1,5 +1,6 @@
 # SCN imports
 import SCN.graphs.permutations as perms
+from SCN.graphs.graph_utlis import save_pickle
 
 # Base python imports
 import multiprocessing
@@ -7,19 +8,29 @@ import multiprocessing
 
 class Random_Graph_Creator:
 
-    def __init__(self, perms: int, name:str='graph') -> None:
+    '''
+    Class to permuate random graphs for network assumptions.
+
+    Parameters
+    ----------
+    perms: int of number of permutations
+    *data: thresholded graphs 
+
+    Usage
+    -----
+    from SCN.assumptions.random_graph_creation import Random_Graph_Creator
+
+    graph = Random_Graph_Creator(1000)
+    graph.data(group_one_graph_threshold, group_two_graph_threshold, group_three_graph_threshold)
+    graph.permutations()
+
+    '''
+    
+    def __init__(self, perms: int) -> None:
         
         self.perms = perms
-        self.name = name
 
     def data(self, *data) -> None:
-        '''
-        Method to read in data
-
-        Parameters
-        ----------
-        *data: thresholded graphs 
-        '''
 
         self.data = dict(zip([f'group_{key}'for key in range(len(data))], [group for group in data]))
         self.groups = len(self.data.keys())
@@ -29,9 +40,10 @@ class Random_Graph_Creator:
         print(f'\nCreating Random graph for {key}\n')
         graph_threshold = self.data[key]
         perms.random_graph_permutations(graph_threshold, self.perms, key)
-    
+
     def permutations(self):
 
         with multiprocessing.Pool(self.groups) as func:
             func.map(self.muti_processing_permutations, self.data.keys())
+
         
