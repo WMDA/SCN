3f932c5548322d45516910e2862da1adb12f6c58
wmda <p3dh0001@gmail.com>
2022-09-13 16:58:48 +0100

    Started on class to calculate test-statistic

--- SCN/group_differences/group_differences.py
+++ SCN/group_differences/group_differences.py
@@ -5,6 +5,7 @@ from SCN.graphs.graph_utlis import save_pickle
 # Base python imports
 import multiprocessing
 
+
 def list_of_measures() -> list:
     '''
     Function to get list of graph measures
@@ -39,7 +40,7 @@ class Create_thresholded_graphs:
         self.results = self.results_dictionary_creation(*data)
         self.names = graphs.load_names()
         self.centroids = graphs.load_centroids()
- 
+
     def data_dictionary(self, *data) -> dict:
         return dict(zip([f'group_{key}'for key in range(len(data))], [group for group in data]))
 
@@ -51,22 +52,35 @@ class Create_thresholded_graphs:
 
         for threshold in self.thresholds:
             data = self.data_dic[key]
-            graph = graphs.create_graphs(data, self.names, self.centroids, threshold)
+            graph = graphs.create_graphs(
+                data, self.names, self.centroids, threshold)
             graph['graph_threshold'].__name__ = f'{key}_graph_threshold_value_{threshold}'
             self.results[f'{key}'].append(graph['graph_threshold'])
 
     def create(self) -> None:
-        for key in self.data_dic.keys() :
+        for key in self.data_dic.keys():
             self.thresholding_graphs(key)
 
     def global_measures(self) -> dict:
-        
-        print('Calculating Global measures for each thresholded graph')
+
+        print('Calculating global measures for each thresholded graph')
         global_measures_dict = {}
         for keys, values in self.results.items():
             for graph_object in values:
                 global_measures = graph_object.calculate_global_measures()
                 global_measures_dict[f'{graph_object.__name__}'] = global_measures
-        
+
         return global_measures_dict
 
+
+class Test_statstic:
+    def __init__(self, global_measures: dict) -> None:
+        self.global_measures = global_measures
+        self.measures = list_of_measures()
+
+    def create_group_dictionary(self):
+
+        import re
+        cleaned_keys = [re.sub(r'_graph_threshold_value_.*', '', key) for key in list(self.global_measures.keys())]
+        groups = list(set(cleaned_keys))
+        return dict(zip(groups, [list() for group in groups]))
